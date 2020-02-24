452e7eacc892a3f056af28b129d089121cdbe87b
Andrey Pokrovsky <iampokrovsky@gmail.com>
2020-02-24 14:28:36 +0300

    Добавляет навигацию по форме модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,9 @@
 var navButton = document.querySelector(".main-nav__button");
 var nav = document.querySelector(".main-nav__content");
-var orderButton = document.querySelectorAll(".button--order");
+var orderButtons = document.querySelectorAll(".button--order");
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".page__overlay");
+var modalFormInputS = document.querySelector(".modal__form-input--s");
 
 document.addEventListener('DOMContentLoaded', function () {
   if (navButton.classList.contains("main-nav__button--no-js") && nav.classList.contains("main-nav__content--no-js")) {
@@ -23,18 +24,32 @@ navButton.onclick = function (evt) {
   }
 };
 
-orderButton.onclick = function (evt) {
-  evt.preventDefault();
+orderButtons.forEach(function (button) {
+  button.onclick = function (evt) {
+    evt.preventDefault();
 
-  if (!modal.classList.contains("modal--open") && !overlay.classList.contains("page__overlay--open")) {
-    modal.classList.add("modal--open");
-    overlay.classList.add("page__overlay--open");
+    if (!modal.classList.contains("modal--open") && !overlay.classList.contains("page__overlay--open")) {
+      modal.classList.add("modal--open");
+      overlay.classList.add("page__overlay--open");
+      modalFormInputS.focus();
+    }
   }
-};
+});
 
 overlay.onclick = function (evt) {
   if (modal.classList.contains("modal--open") && overlay.classList.contains("page__overlay--open")) {
     modal.classList.remove("modal--open");
     overlay.classList.remove("page__overlay--open");
   }
-}
+};
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+
+    if (modal.classList.contains("modal--open") && overlay.classList.contains("page__overlay--open")) {
+      modal.classList.remove("modal--open");
+      overlay.classList.remove("page__overlay--open");
+    }
+  }
+});
