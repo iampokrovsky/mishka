07c745e971432834f0c593ea2db87607e359b68b
Andrey Pokrovsky <iampokrovsky@gmail.com>
2020-02-27 13:47:46 +0300

    Добавляет слайдер для блока с отзывами

---
+++ source/libs/swiper-5.3.1/scripts/build-config.js
@@ -0,0 +1,42 @@
+// module.exports = {
+//   components: [
+//     'virtual',
+//     'keyboard',
+//     'mousewheel',
+//     'navigation',
+//     'pagination',
+//     'scrollbar',
+//     'parallax',
+//     'zoom',
+//     'lazy',
+//     'controller',
+//     'a11y',
+//     'history',
+//     'hash-navigation',
+//     'autoplay',
+//     'effect-fade',
+//     'effect-cube',
+//     'effect-flip',
+//     'effect-coverflow',
+//     'thumbs',
+//   ],
+//   themeColor: '#007aff',
+//   colors: {
+//     white: '#ffffff',
+//     black: '#000000',
+//   },
+// };
+
+module.exports = {
+  components: [
+    'keyboard',
+    'navigation',
+    'a11y',
+    'autoplay',
+  ],
+  themeColor: '#000000',
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+  },
+};
