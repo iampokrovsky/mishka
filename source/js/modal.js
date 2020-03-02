44890cf15ef0807ec5f168708bfbdadbd9986a3e
Andrey Pokrovsky <iampokrovsky@gmail.com>
2020-03-02 12:50:58 +0300

    Добавляет скрипт для модального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,19 +1,17 @@
-var orderButtons = document.querySelectorAll(".button--order");
+var orderButton = document.querySelector(".button--order");
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".page__overlay");
 var modalFormInputS = document.querySelector(".modal__form-input--s");
 
-orderButtons.forEach(function (button) {
-  button.onclick = function (evt) {
-    evt.preventDefault();
+orderButton.onclick = function (evt) {
+  evt.preventDefault();
 
-    if (!modal.classList.contains("modal--open") && !overlay.classList.contains("page__overlay--open")) {
-      modal.classList.add("modal--open");
-      overlay.classList.add("page__overlay--open");
-      modalFormInputS.focus();
-    }
+  if (!modal.classList.contains("modal--open") && !overlay.classList.contains("page__overlay--open")) {
+    modal.classList.add("modal--open");
+    overlay.classList.add("page__overlay--open");
+    modalFormInputS.focus();
   }
-});
+}
 
 overlay.onclick = function (evt) {
   if (modal.classList.contains("modal--open") && overlay.classList.contains("page__overlay--open")) {
